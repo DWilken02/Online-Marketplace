898d11863cc8f626753593f9a475dd4afaed2e8e
DWilken02 <Dylan.Wilken06@gmail.com>
2023-10-06 15:35:03 -0700

    simplify JavaScript and HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,37 @@
+//UI Logic
 window.addEventListener("load", function() {
-  document.getElementById("radio-form").addEventListener("submit", handleRadio);
-  document.getElementById("payment-info").addEventListener("submit", paymentInfo);
-  document.getElementById("shipping-info").addEventListener("submit", shippingInfo);
+  document.getElementById("order-form").addEventListener("submit", handleOrderFormSubmission);
 });
 
-
-function handleRadio(event) {
+function handleOrderFormSubmission(event) {
   event.preventDefault();
   const radioSelection = document.querySelector("input[name='product']:checked").value;
-  document.querySelector("span#product-display").innerText= radioSelection;
-  if (radioSelection == "Necklace") {
-    let productTotal = applyDiscount(100, .1)
-    document.querySelector("span#price-display").innerText = "$" + productTotal;
-  } else if (radioSelection == "Earrings") {
-    let productTotal = applyDiscount(200, .2)
-    document.querySelector("span#price-display").innerText = "$" + productTotal;
-  } else if (radioSelection == "Bracelet") {
-    let productTotal = applyDiscount(300, .3)
-    document.querySelector("span#price-display").innerText = "$" + productTotal;
-  }
-}
-
-function paymentInfo(event) {
-  event.preventDefault();
   const cardNumberInput = document.getElementById("creditCardNumber").value;
   const cvvInput = document.getElementById("cvv").value;
   const expDateInput = document.getElementById("exp-date").value;
-
-  document.querySelector("span#card-number-display").innerText = cardNumberInput;
-  document.querySelector("span#cvv-display").innerText = cvvInput;
-  document.querySelector("span#exp-date-display").innerText = expDateInput;
-}
-
-function shippingInfo(event) {
-  event.preventDefault();
   const addressInput = document.getElementById("address").value;
   const cityInput = document.getElementById("city").value;
   const stateInput = document.getElementById("state").value;
   const zipCodeInput = document.getElementById("zip-code").value;
 
+  document.querySelector("span#product-display").innerText= radioSelection;
+  if (radioSelection == "Necklace") {
+    document.querySelector("span#price-display").innerText = "$" + applyDiscount(100, .1);
+  } else if (radioSelection == "Earrings") {
+    document.querySelector("span#price-display").innerText = "$" + applyDiscount(200, .2);
+  } else if (radioSelection == "Bracelet") {
+    document.querySelector("span#price-display").innerText = "$" + applyDiscount(300, .3);
+  }
+  document.querySelector("span#card-number-display").innerText = cardNumberInput;
+  document.querySelector("span#cvv-display").innerText = cvvInput;
+  document.querySelector("span#exp-date-display").innerText = expDateInput;
   document.querySelector("span#address-display").innerText = addressInput;
   document.querySelector("span#city-display").innerText = cityInput;
   document.querySelector("span#state-display").innerText = stateInput;
   document.querySelector("span#zip-code-display").innerText = zipCodeInput;
 }
 
+// Business Logic
 function applyDiscount(productPrice, discount) {
   return productPrice - (productPrice * discount); 
 }
