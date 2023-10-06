73fb2c5c3c0646c32c12ea7a009536351a97f353
DWilken02 <Dylan.Wilken06@gmail.com>
2023-10-06 15:09:34 -0700

    update JavaScript and HTML

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,27 @@ function handleRadio(event) {
   event.preventDefault();
   const radioSelection = document.querySelector("input[name='product']:checked").value;
   document.querySelector("span#product-display").innerText= radioSelection;
+  if (radioSelection == "Necklace") {
+    let productTotal = applyDiscount(100, .1)
+    document.querySelector("span#price-display").innerText = "$" + productTotal;
+  } else if (radioSelection == "Earrings") {
+    let productTotal = applyDiscount(200, .2)
+    document.querySelector("span#price-display").innerText = "$" + productTotal;
+  } else if (radioSelection == "Bracelet") {
+    let productTotal = applyDiscount(300, .3)
+    document.querySelector("span#price-display").innerText = "$" + productTotal;
+  }
 }
 
 function paymentInfo(event) {
   event.preventDefault();
-  //finish code
+  const cardNumberInput = document.getElementById("creditCardNumber").value;
+  const cvvInput = document.getElementById("cvv").value;
+  const expDateInput = document.getElementById("exp-date").value;
+
+  document.querySelector("span#card-number-display").innerText = cardNumberInput;
+  document.querySelector("span#cvv-display").innerText = cvvInput;
+  document.querySelector("span#exp-date-display").innerText = expDateInput;
 }
 
 function shippingInfo(event) {
@@ -27,4 +43,8 @@ function shippingInfo(event) {
   document.querySelector("span#city-display").innerText = cityInput;
   document.querySelector("span#state-display").innerText = stateInput;
   document.querySelector("span#zip-code-display").innerText = zipCodeInput;
+}
+
+function applyDiscount(productPrice, discount) {
+  return productPrice - (productPrice * discount); 
 }
